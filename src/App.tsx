f74f11097c04e3aa3def2049f90002373dfae736
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-26 13:38:51 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,39 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+// type タイプ名
+// セミコロンでくぐる
+// ? をつけることで型指定が必須じゃないことを明示
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed?: boolean;
+};
+
 export default function App() {
   // 空の配列を初期値としている
   // stateに型を定義
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
   const onClickFeachData = () => {
     // .then() ~ 取得後の関数を記述できる
     // todoデータの配列をステートに保持して、todo一覧を表示する
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      // ステートを更新
-      setTodos(res.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        // ステートを更新
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFeachData}>APIデータを取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userid} />
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
         // <>
         //   <p>{todo.id}</p>
         //   <p>{todo.title}</p>
