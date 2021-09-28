6161d294c2b0623e4a3bca66df11c52dce40de70
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-29 02:15:25 +0900

    initial commit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,15 +1,14 @@
 // * propsの型を定義 *
 
-// 型を定義
-type TodoType = {
-  userId: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
 
 // propsにTodoTypeの型を定義
 // propsに初期値をいれる事が可能
-export const Todo = (props: TodoType) => {
+// Pick<第一引数に型, 第２引数に必要な値だけ記述> で型の使い回しができる
+// omit<第一引数に型, 第２引数> Pickの逆で第２引数以外の方が適用される
+export const Todo = (
+  props: Pick<TodoType, "userId" | "title" | "completed"> //id以外を指定している
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
